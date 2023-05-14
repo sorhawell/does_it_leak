1d8f82a8dc6b1c7102946742f4cd668cd01156ae
sorhawell <sorhawell@gmail.com>
2023-05-14 22:35:05 +0200

    Uobj

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,10 +1,23 @@
 use extendr_api::prelude::*;
 
+#[derive(Clone)]
 struct MyString {
     pub s: String,
 }
 
-#[extendr(r_class_name = "BobbyJoeString")]
+struct Uobj(Robj);
+impl From<Robj> for Uobj {
+    fn from(x: Robj) -> Self {
+        Uobj(x)
+    }
+}
+impl From<Uobj> for Robj {
+    fn from(x: Uobj) -> Self {
+        x.0
+    }
+}
+
+#[extendr(r_class_name = "BobbyJoeString", r_super_class_name = "AmericanString")]
 impl MyString {
     pub fn new() -> MyString {
         MyString { s: "im new".into() }
@@ -12,7 +25,24 @@ impl MyString {
     pub fn print(&self) {
         println!("mystring is {}", &self.s);
     }
+
+    pub fn show(ms: &MyString, s: String) -> std::result::Result<MyString, String> {
+        ms.print();
+        println!("{}", s);
+        Ok(ms.clone())
+    }
 }
+
+// impl<'a> extendr_api::FromRobj<'a> for BobbyJoeString {
+//     fn from_robj(robj: &'a Robj) -> std::result::Result<Self, &'static str> {
+//         if robj.check_external_ptr_type::<BobbyJoeString>() {
+//             #[allow(clippy::transmute_ptr_to_ref)]
+//             Ok(unsafe { std::mem::transmute(robj.external_ptr_addr::<BobbyJoeString>()) })
+//         } else {
+//             Err(concat!("expected ", ""))
+//         }
+//     }
+// }
 
 // Macro to generate exports.
 // This ensures exported functions are registered with R.
