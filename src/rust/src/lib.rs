eb38aca865aaee28b23f28298927c15387a35e24
sorhawell <sorhawell@gmail.com>
2023-05-16 09:02:05 +0200

    more uobj trait impls

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -2,10 +2,10 @@ use extendr_api::prelude::*;
 
 #[derive(Clone)]
 struct MyString {
-    pub s: String,
+    pub s: i32,
 }
 
-struct Uobj(Robj);
+pub struct Uobj(Robj);
 impl From<Robj> for Uobj {
     fn from(x: Robj) -> Self {
         Uobj(x)
@@ -17,30 +17,78 @@ impl From<Uobj> for Robj {
     }
 }
 
+impl From<&Robj> for &Uobj {
+    fn from(x: &Robj) -> Self {
+        let y: &Uobj = unsafe { std::mem::transmute(x) }; // it is a tupple struct
+        y
+    }
+}
+
+impl TryFrom<Uobj> for String {
+    type Error = extendr_api::error::Error;
+    fn try_from(x: Uobj) -> std::result::Result<Self, Error> {
+        Ok(x.0.as_str().unwrap().to_string())
+    }
+}
+
+impl TryFrom<&Uobj> for &str {
+    type Error = Error;
+    fn try_from(robj: &Uobj) -> extendr_api::Result<Self> {
+        let robj = &robj.0; //SOREN comment unpack
+        if robj.is_na() {
+            return Err(Error::MustNotBeNA(robj.clone()));
+        }
+        match robj.len() {
+            0 => Err(Error::ExpectedNonZeroLength(robj.clone())),
+            1 => {
+                if let Some(s) = robj.as_str() {
+                    Ok(s)
+                } else {
+                    Err(Error::ExpectedString(robj.clone()))
+                }
+            }
+            _ => Err(Error::ExpectedScalar(robj.clone())),
+        }
+    }
+}
+
+//converted from extendr try_from_robj.rs
+impl TryFrom<&Uobj> for String {
+    type Error = Error;
+    fn try_from(uobj: &Uobj) -> Result<Self> {
+        <&str>::try_from(uobj).map(|s| s.to_string())
+    }
+}
+
+// fn user_func(robj: Robj) -> Robj {
+//     robj
+// }
+
 #[extendr(r_class_name = "BobbyJoeString", r_super_class_name = "AmericanString")]
 impl MyString {
     pub fn new() -> MyString {
-        MyString { s: "im new".into() }
+        MyString { s: 42 }
     }
     pub fn print(&self) {
         println!("mystring is {}", &self.s);
     }
 
-    pub fn show(ms: &MyString, s: String) -> std::result::Result<MyString, String> {
-        ms.print();
+    pub fn show(s: String) -> () {
         println!("{}", s);
-        Ok(ms.clone())
     }
 }
 
-// impl<'a> extendr_api::FromRobj<'a> for BobbyJoeString {
-//     fn from_robj(robj: &'a Robj) -> std::result::Result<Self, &'static str> {
-//         if robj.check_external_ptr_type::<BobbyJoeString>() {
-//             #[allow(clippy::transmute_ptr_to_ref)]
-//             Ok(unsafe { std::mem::transmute(robj.external_ptr_addr::<BobbyJoeString>()) })
-//         } else {
-//             Err(concat!("expected ", ""))
-//         }
+pub trait FromUobj: Sized {
+    // Convert an incomming Robj from R into a value or an error.
+    fn from_uobj(_robj: Uobj) -> std::result::Result<Self, String> {
+        Err("unable to convert value from R object".into())
+    }
+}
+
+// impl FromUobj for MyString {
+//     fn from_uobj(uobj: Uobj) -> std::result::Result<Self, String> {
+//         let robj = &uobj.0;
+//         MyString::from_robj(robj)
 //     }
 // }
 
